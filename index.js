c37c91025f5cd0c4e4212752ea4b62baf1165ebc
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-09-16 17:54:46 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    hola: 'mundo',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
