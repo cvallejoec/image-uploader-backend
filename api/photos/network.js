0fc4e2658d5777f7c4d9c4708cabbc697c1ac779
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-09-16 19:26:29 -0500

    Better structure for multer files

--- api/photos/network.js
+++ api/photos/network.js
@@ -1,23 +1,8 @@
 const express = require('express');
-const path = require('path');
 const router = express.Router();
-const multer = require('multer');
+const upload = require('../../utils/multer/config');
 
 const Controller = require('./controller');
-
-// Multer Configuration
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, '/../../uploads'));
-  },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      file.fieldname + '-' + Date.now() + path.extname(file.originalname)
-    );
-  },
-});
-const upload = multer({ storage });
 
 // Routes
 router.get('/:id', get);
