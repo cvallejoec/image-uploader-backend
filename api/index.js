77ad40cc49292fc038d196c34e93aed08e021ddb
Carlos Vallejo <cvallejo.ec@gmail.com>
2020-09-16 19:47:39 -0500

    Configure Vercel and CORS

--- api/index.js
+++ api/index.js
@@ -3,6 +3,17 @@ const app = express();
 
 const uploader = require('./photos/network');
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+  );
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
+
 app.use('/api/uploader', uploader);
 
 app.listen(3000, () => {
